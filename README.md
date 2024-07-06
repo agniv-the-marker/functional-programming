cd2856a94d0beb7b1a9b6f84349a1a1ea60ef92e
Agniv Sarkar <agni.spare@gmail.com>
2024-07-06 01:22:55 -0700

    Automatic Links

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Won't you shake a poor Haskell's hand...?
 
 This is the GitHub sync I've setup for my learning haskell, see [[Functional Programming]] for more! This will allow the website at [haskell.agniv.me](https://haskell.agniv.me) to have linked code, since the plug-in I'm using for this site currently doesn't love it.
+
+Make sure to utilize [[re_linker.py]] before pushing this repository to the website, as it will fix links to code and redirect them here!
---
+++ re_linker.py
@@ -0,0 +1,33 @@
+"""
+Replaces each [[file_name]] with [file_name](github link),
+which fixes the broken links in the Obsidian vault.
+"""
+import glob
+import json
+import os
+
+DIRECTORY = './'
+BASE_URL = 'https://github.com/agniv-the-marker/functional-programming/blob/main'
+PLUGIN_PATH = './.obsidian/plugins/digitalgarden/data.json'
+
+if __name__ == "__main__":
+    files = glob.glob(DIRECTORY + '/**/[!.]*[!~].*', recursive=True)
+    non_markdown = []
+    for file in files:
+        if file.endswith('.md'):
+            continue
+        file = file.replace('\\', '/')
+        non_markdown.append(file[1:])
+    with open(PLUGIN_PATH, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        data["customFilters"] = []
+        for file in non_markdown:
+            file_name = file.split('/')[-1]
+            data["customFilters"].append({
+                'pattern': "\\[\\[" + file_name + "\\]\\]",
+                'flags': 'g',
+                'replace': f'[{file_name}]({BASE_URL}{file})'
+            })
+    os.remove(PLUGIN_PATH)
+    with open(PLUGIN_PATH, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4)
